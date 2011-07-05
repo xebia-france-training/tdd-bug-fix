1ae2581caf16d7c3202804ed2066af6464d26789
simcap <caplette@hotmail.co.uk>
2011-07-05 22:07:55 +0200

    adding exercises rules

--- src/fr/xebia/workshop/tdd/bugfix/UserBookingService.java
+++ src/fr/xebia/workshop/tdd/bugfix/UserBookingService.java
@@ -4,6 +4,21 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Exercise:
+ * --------
+ * 
+ * Fix the bugs in this order using TDD. Always write a test first before touching the code.
+ * 
+ * Bug 1. When I tried to retrieve the shows that are below or equal to 100 I get only 2 shows
+ * when there should be 3 shows.
+ * 
+ * Bug 2. When I tried to retrieve the shows that are below or equal to 110 I get only 2 shows
+ * when I expect 4 shows.
+ * 
+ * Bug 3. When I calculate the basket price I get numbers with lot of decimals 
+ * when I expect a price with a format as following 256.00 (i.e. 2 decimals) 
+ */
 public class UserBookingService {
 
 	private ShowDao showDao = new ShowDao();
